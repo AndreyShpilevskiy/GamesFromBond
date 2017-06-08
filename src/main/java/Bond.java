1b21bbaa0c9f9aa42064baa4a82160e3277a1443
andrej <andrej.shpilevskij@gmail.com>
2017-06-08 15:01:05 +0300

    First5

--- src/main/java/Bond.java
+++ src/main/java/Bond.java
@@ -4,12 +4,6 @@ public class Bond {
 
     public static void main(String[] args) throws InterruptedException {
 
-//        System.setProperty("webdriver.gecko.driver", "C://IT/Test/geckodriver.exe");
-//        DesiredCapabilities capabilities = DesiredCapabilities.firefox();
-//        capabilities.setCapability("marionette", true);
-//        WebDriver driver = new FirefoxDriver(capabilities);
-//        WebDriverWait wait = new WebDriverWait(driver, 40);
-
         int a = 1;
         while (a == 1) {
 
